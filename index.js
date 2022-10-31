3c57768fec5a15c8b540b858551f577b8fd8f710
Chandler Dearden <chandlerjdearden@gmail.com>
2022-10-31 16:03:04 -0600

    'HTML and Css'

---
+++ index.js
@@ -0,0 +1,10 @@
+let frenchimg = document.querySelector(".frenchimg")
+
+function addfrenchToast () {
+    let frenchToast = document.createElement('h2')
+    frenchToast.textContent = ('French Toast!')
+    foodName.appendChild.add(frenchToast)
+}
+
+
+frenchimg.addEventListener('click', addfrenchToast)
